e01c87e42dba0a38f76df564417b7adc0c3b6416
Rui Yang <reyyang00@gmail.com>
2021-06-22 18:36:17 -0700

    add disk image

--- profile.py
+++ profile.py
@@ -21,7 +21,9 @@ request = pc.makeRequestRSpec()
 node = request.RawPC("node")
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+#node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
 
+# Request that a specific image be installed on this node
+node.disk_image = "urn:publicid:IDN+utah.cloudlab.us+image+flsys-PG0:SSDCaProfile-disk-image"
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
